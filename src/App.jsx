ff2d6185916313f0e3e6c22fc739872a411486db
mahakmd <mandegarimahak@gmail.com>
2024-04-11 03:37:05 +0330

    form react js

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Forms from "./components/Forms";
+import './App.css';
+import Captcha from "./components/Captcha";
+
+export default function App() {
+  return (
+    <div dir="rtl">
+       <Forms />
+       {/* <Captcha /> */}
+    </div>
+  )
+}
